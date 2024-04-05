e3cedb3b608e6d9dcadf79f19920e79be6c2d882
Scr0mble <pitchfor@grinnell.edu>
2024-04-04 21:10:09 -0500

    added start of insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -33,6 +33,19 @@ public class InsertionSort implements Sorter {
 
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
+    for(int i = 0; i < values.length; i++) {
+      for(int j = 0; j < i; j++) {
+        int pos = order.compare(values[i], values[j]);
+        if(pos < 0) {
+          T temp = values[j];
+          values[j] = values[i];
+          for(int k = j; k < i; k++){
+            values[k+1] = values[k];
+            values[k] = temp;
+
+          }
+        }
+      }//inner for loop
+    }//outer for loop
   } // sort(T[], Comparator<? super T>
 } // class InsertionSort
