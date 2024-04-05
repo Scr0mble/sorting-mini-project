57a6391f0eb0559cfa750121838096427384c726
William <pitchfor@grinnell.edu>
2024-04-04 23:41:46 -0500

    added partition to Quicksort, also added more tests to SortTester

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -31,6 +31,30 @@ public class Quicksort implements Sorter {
   // | Methods |
   // +---------+
 
+  public <T> void swap(T[] values, int left, int right) {
+    T temp = values[left];
+    values[left] = values[right];
+    values[right] = temp;
+  } //swap(T[], int, int)
+
+  public <T> void partition(T[] values, Comparator<? super T> order, int lb, int ub) {
+    swap(values, lb, (lb + (ub-lb) / 2));
+
+    int small = lb+1;
+    int large = ub;
+
+    while(small != large) {
+      int pos = order.compare(values[small], values[lb]);
+
+      if(pos < 0){
+        small++;
+      } else if(pos > 0) {
+        swap(values, large-1, small);
+        large--;
+      }
+    }
+  }
+
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
     // STUB
--- src/SortTester.java
+++ src/SortTester.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import java.math.BigInteger;
+import java.util.Comparator;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -17,6 +18,9 @@ public class SortTester {
   // +---------+-----------------------------------------------------
   // | Globals |
   // +---------+
+
+  Comparator <Integer> intComp = (x,y) -> x == y ? 0 : x < y ? -1 : 1;
+  Comparator <String> strComp = (str1,str2) -> str1.compareTo(str2); 
 
   Sorter sorter;
 
@@ -40,6 +44,46 @@ public class SortTester {
   @Test
   public void reverseOrderedStringTest() {
     String[] original = { "foxtrot", "delta", "charlie", "bravo", "alpha" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void emptyListTest() {
+    String[] original = {};
+    String[] expected = {};
+    sorter.sort(original, strComp);
+    assertArrayEquals(original, expected);
+  } //empty list test
+
+  @Test
+  public void reverseOrderedIntTest() {
+    Integer[] original = { 5, 4, 3, 2, 1 };
+    Integer[] expected = { 1, 2, 3, 4, 5 };
+    sorter.sort(original, intComp);
+    assertArrayEquals(original, expected);
+  }
+
+  @Test
+  public void orderedIntTest() {
+    Integer[] original = { 1, 2, 3, 4, 5 };
+    Integer[] expected = { 1, 2, 3, 4, 5 };
+    sorter.sort(original, intComp);
+    assertArrayEquals(original, expected);
+  }
+
+  @Test
+  public void randOrderedIntTest() {
+    Integer[] original = { 2, 4, 3, 5, 1 };
+    Integer[] expected = { 1, 2, 3, 4, 5 };
+    sorter.sort(original, intComp);
+    assertArrayEquals(original, expected);
+  }
+
+  @Test
+  public void randOrderedStringTest() {
+    String[] original = { "delta", "alpha", "foxtrot", "bravo", "charlie" };
     String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
     sorter.sort(original, (x, y) -> x.compareTo(y));
     assertArrayEquals(original, expected);
