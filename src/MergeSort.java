6b7b8e2d7eb7209da0ca315ad84d1f95d4d2649d
Scr0mble <pitchfor@grinnell.edu>
2024-04-06 19:58:52 -0500

    Added merge sort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
@@ -31,8 +32,41 @@ public class MergeSort implements Sorter {
   // | Methods |
   // +---------+
 
+  public <T> void merge(T[] vals, int lb, int mid, int ub, Comparator<? super T> comparator) {
+    T[] temp = Arrays.copyOfRange(vals, 0, vals.length);
+    int mergePos = lb;
+    int lo = lb;
+
+    while(lo != mid) {
+      int pos = comparator.compare(vals[lb], vals[mid]);
+
+      if(pos < 0) {
+        temp[mergePos] = vals[lo];
+        lo++;
+      } else {
+        temp[mergePos] = vals[mid];
+        mid++;
+      }//if else statement
+
+      for(int i = lb; i < ub; i++) {
+        vals[i] = temp[i];
+      }//for loop
+    }//While loop
+    return;
+  }
+
+  public <T> void mergesort(T[] values, Comparator<? super T> comparator, int lb, int ub) {
+    if((ub-lb) <= 1) {
+      return;
+    } else {
+      merge(values, lb, (lb+(ub-lb)/2), ub, comparator);
+      mergesort(values, comparator, lb, (lb+(ub-lb)/2));
+      mergesort(values, comparator, (lb+(ub-lb)/2)+1, ub);
+    }
+  }
+
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
+    mergesort(values, order, 0, values.length);
   } // sort(T[], Comparator<? super T>
 } // class MergeSort
