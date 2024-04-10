8a54f067d17b745968bc15bd0e376df1a8aa7b74
Scr0mble <pitchfor@grinnell.edu>
2024-04-10 00:31:49 -0500

    updated read me, made WPsort work

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # sorting-mini-project
 As the name suggests, it's our mini-project for the sorting lab.
-Authors: William Pitchford, Sam R. Purpose: To implement sorting algorithms and understand their runtimes. Acknowledgements: Jayson Kunkel, helped me understand how quick and merge sort were supposed to recurse. GitHub link https://github.com/Scr0mble/sorting-mini-project
+Authors: William Pitchford, Sam R. Purpose: To implement sorting algorithms and understand their runtimes. Acknowledgements: Jayson Kunkel, helped me understand how quick and merge sort were supposed to recurse. GitHub link https://github.com/Scr0mble/sorting-mini-project, also ChatGPT for helping me find and write my sort, which is just an implementation of BucketSort.
--- src/WilliamPitchfordSort.java
+++ src/WilliamPitchfordSort.java
@@ -2,6 +2,42 @@
  * @author William Pitchford
  * Purpose: Trying to implement a really fast sorting method
  */
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class WilliamPitchfordSort {
-  //I'll finish this later, sorry Sam!
+
+  public static <T extends Comparable<T>> void bucketSort(T[] arr) {
+    if (arr == null || arr.length == 0) {
+        return;
+    }
+
+    int n = arr.length;
+    @SuppressWarnings("unchecked")
+    ArrayList<T>[] buckets = new ArrayList[n];
+
+    // Create empty buckets
+    for (int i = 0; i < n; i++) {
+        buckets[i] = new ArrayList<>();
+    }
+
+    // Add elements into buckets
+    for (int i = 0; i < n; i++) {
+        int bucketIndex = (int) (n * arr[i].compareTo(arr[0]));
+        buckets[bucketIndex].add(arr[i]);
+    }
+
+    // Sort individual buckets
+    for (int i = 0; i < n; i++) {
+        Collections.sort(buckets[i]);
+    }
+
+    // Concatenate all buckets into arr[]
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < buckets[i].size(); j++) {
+            arr[index++] = buckets[i].get(j);
+        }
+    }
+}
 }
